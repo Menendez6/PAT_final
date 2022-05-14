28c6e537260e08fd3389a2d8b37168a6fd7b28c5
alexalzaga <201804530@alu.comillas.edu>
2022-05-14 19:46:34 +0200

    0.2 - confirmado & admin menus empezado

--- src/main/resources/static/js/admin_menu.js
+++ src/main/resources/static/js/admin_menu.js
@@ -16,20 +16,28 @@ async function mostrarRestaurantes(){
             temp += '<td>' + data[i].id + '</td>';
             temp += '<td>' + data[i].restName + '</td>';
             temp += '<td>' + data[i].direccion + '</td>';
-            temp += '<td class="text-center"><button class="btn btn-outline-dark mt-auto py-1 px-4 mt-auto" id="' + data[i].id +'">Ver</button></td></tr>';
+            temp += '<td class="text-center" id="boton"></td></tr>';
 
-            document.getElementById(data[i].id).addEventListener("click",function(event){
-                restaurante=data[i].id;
+            document.getElementById('data_res').innerHTML += temp; // metemos las filas ya modificadas
+
+            var boton = document.createElement("a");
+            boton.className = "btn btn-outline-dark mt-auto py-1 px-4";
+            boton.id = data[i].id;
+            boton.onclick = "restaurante="+data[i].id;
+            boton.innerHTML = "Ver";
+
+            boton.addEventListener("click",function(event){
+                mostrarPlatos();
             })
-        }
-         document.getElementById('data_res').innerHTML = temp; // metemos las filas ya modificadas
 
+            document.getElementById('boton').appendChild(boton);
+            }
         }
     })
 }
 
 function mostrarPlatos() {
-    url = "/api/platos/"+restaurante;
+    url = "/api/platos/"+localStorage.getItem('restaurante');
     fetch(url)
     .then(response => response.json())
     .then(data_p => {
@@ -38,18 +46,19 @@ function mostrarPlatos() {
 
         if (data_p.length > 0) {
           var temp2 = '';
+          document.getElementById('tabla_platos').style.visibility = "visible";
           document.getElementById('data_platos').innerHTML = temp2; //vaciamos las rows previas
           for (var i = 0; i < data_p.length; i++) { // rellenamos fila por fila
-            var imagen += datos[i].foto;
-            temp += '<tr>';
-            temp += '<td><img src="'+ imagen +'" height="120px" width="auto"></td>';
+            var imagen = data_p[i].foto;
+            temp2 += '<tr>';
+            temp2 += '<td><img src="'+ imagen +'" height="120px" width="auto"></td>';
             temp2 += '<td>' + data_p[i].id_plato + '</td>';
             temp2 += '<td>' + data_p[i].nombre + '</td>';
             temp2 += '<td>' + data_p[i].descripcion + '</td>';
             temp2 += '<td>' + data_p[i].seccion + '</td>';
             temp2 += '<td>' + data_p[i].precio + '</td>';
-             temp2 += '<td class="text-center"><button class="btn btn-outline-dark mt-auto py-1 px-4 mt-auto" id="modify-' + data_p[i].id_plato +'">Modificar</button>';
-            temp2 += '<button class="btn btn-danger btn-outline-dark mt-auto py-1 px-4 mt-auto" id="delete-' + data_p[i].id_plato +'">Eliminar</button></td></tr>';
+            temp2 += '<td class="text-center"><button class="btn btn-outline-dark mt-auto py-1 px-3 mt-auto" id="modify-' + data_p[i].id_plato +'">Modificar</button>';
+            temp2 += '<button class="btn btn-danger btn-outline-dark mt-auto py-1 px-3 mt-auto" id="delete-' + data_p[i].id_plato +'">Eliminar</button></td></tr>';
         }
          document.getElementById('data_platos').innerHTML = temp2; // metemos las filas ya modificadas
 
